ad6ab34aca4d30de51bdbfe56f304955e2a7b17a
Arpan Guin <arpguin@gmail.com>
2021-12-22 10:37:24 +1100

    Updated

--- Sorting/BubbleSort.py
+++ Sorting/BubbleSort.py
@@ -6,18 +6,22 @@ Swapping: O(n**2)
 """
 
 
-def BubbleSort(values):
-    length = len(values)
-    while length > 0:
-        for i in range(length - 1):
-            if values[i] > values[i + 1]:
-                temp = values[i]
-                values[i] = values[i + 1]
-                values[i + 1] = temp
-        length -= 1
+def bubbleSort(array):
+    is_sorted = False
+    counter = 0
+    while not is_sorted:
+        index = 0
+        is_sorted = True
+        while index < len(array)-1-counter:	# On every eteration the biggest element is sorted to last, so minusing the last element represented by counter
+            if array[index] > array[index+1]:
+                array[index], array[index+1] =  array[index+1], array[index]
+                is_sorted = False
+            index += 1
+        counter += 1
+    return array
 
 
 values = [3, 5, 8, 9, 6, 2]
 print('Original Array:', values)
-BubbleSort(values)
+bubbleSort(values)
 print('Sorted Array:', values)
