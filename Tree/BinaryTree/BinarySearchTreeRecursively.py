d6aa50eeeb120444069ea316e087f1fcae222ccc
Arpan Guin <arpguin@gmail.com>
2022-01-05 15:29:13 +1100

    Updated

---
+++ Tree/BinaryTree/BinarySearchTreeRecursively.py
@@ -0,0 +1,14 @@
+Perfect binary Tree
+--------------------
+All interior nodes has exact 2 child nodes and leaf nodes are in same depth.
+
+Complete Binary Tree
+---------------------
+All its levels, except possibly except possibly the last, have the maximum number of nodes and
+All the nodes at the last level appears as far left as possible.
+A complete binary tree is a perfect binary tree till l-1 level.
+
+
+Full Binary Tree
+-----------------
+All nodes has exact 2 or 0 child node.
